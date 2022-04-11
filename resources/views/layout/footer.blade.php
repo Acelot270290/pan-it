606bbb22dfcc38dd2936ad9f1e96a0acca1c1261
Alan Diniz <adiniz@girafa.com.br>
2022-04-10 22:40:35 -0300

    subindo layour

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,119 @@
+      <!-- Start Footer -->
+      <footer class="footer_detail">
+        <div class="container">
+            <div class="row pt-5 pb-5">
+                <div class="col-lg-12">
+                    <div class="text-center footer_about">
+                        <h3 class="text-white font-weight-bold mb-0">Let's Get Started Oskar</h3>
+                        <p class="mx-auto mt-4 mb-0">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                    </div>
+                    <div class="text-center mt-4">
+                       <button type="button" class="btn btn-outline-custom btn-rounded" data-toggle="modal" data-target="#exampleModalCenter">Let's Get!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-lg-3">
+                    <h6 class="mt-3 text-white text-uppercase">Important Link</h6>
+                    <ul class="list-unstyled footer_menu_list mt-3">
+                        <li><a href="#">Terms & Condition</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Return Policy</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Sitemap</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <h6 class="mt-3 text-white text-uppercase">Our Pages</h6>
+                    <ul class="list-unstyled footer_menu_list mt-3">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Features</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Client</a></li>
+                        <li><a href="#">Pricing</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <h6 class="mt-3 text-white text-uppercase">Support</h6>
+                    <ul class="list-unstyled footer_menu_list mt-3">
+                        <li><a href="#">Help Center</a></li>
+                        <li><a href="#">Live Chat</a></li>
+                        <li><a href="#">Downloads</a></li>
+                        <li><a href="#">Press Kit</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3">
+                    <h6 class="mt-3 text-white text-uppercase">Locations</h6>
+                    <ul class="list-unstyled footer_menu_list mt-3">
+                        <li><a href="#">Melbourne</a></li>
+                        <li><a href="#">London</a></li>
+                        <li><a href="#">New York</a></li>
+                        <li><a href="#">San Francisco</a></li>
+                        <li><a href="#">Ontario</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="fot_bor"></div>
+            <div class="row pt-3 pb-3">
+                <div class="col-lg-12">
+                    <div class="float-left float_none">
+                        <p class="copy-rights mb-0"><a target="_blank" href="https://www.templateshub.net">Templates Hub</a></p>
+                    </div>
+                    <div class="float-right float_none">
+                        <ul class="list-inline fot_social mb-0">
+                            <li class="list-inline-item"><a href="#" class="social-icon text-muted"><i class="mdi mdi-facebook"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class="social-icon text-muted"><i class="mdi mdi-twitter"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class="social-icon text-muted"><i class="mdi mdi-linkedin"></i></a></li>
+                            <li class="list-inline-item"><a href="#" class="social-icon text-muted"><i class="mdi mdi-google-plus"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>                
+        </div>
+    </footer>
+    <!-- End Footer -->
+
+    <!-- Back to top -->
+    <a href="#" class="back_top" style="display: inline;"> <i class="pe-7s-up-arrow"> </i> </a>
+        
+    <!-- JAVASCRIPTS -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+
+    <!-- scrollspy js -->
+    <script src="js/scrollspy.min.js"></script>
+
+    <!-- isotope js -->
+    <script src="js/isotope.js"></script>
+
+    <!-- magnific js -->
+    <script src="js/jquery.magnific-popup.min.js"></script>
+
+    <!-- Particles Js -->
+    <script src="js/particles.js"></script>  
+    <script src="js/particles.app.js"></script>
+
+    <!-- isotope js -->
+    <script src="js/isotope.js"></script>
+
+    <!-- custom js -->
+    <script src="js/custom.js"></script>
+
+    <script>
+        $(".element").each(function() {
+            var $this = $(this);
+            $this.typed({
+                strings: $this.attr('data-elements').split(','),
+                typeSpeed: 100,
+                backDelay: 3000
+            });
+        });
+    </script>
+
+</body>
+
+
+<!-- solid_405:49-->
+</html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+    
+<!-- solid_405:48-->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Oskar | Responsive Bootstrap 4 Landing Template</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <meta name="author" content="CreataThemes" />
+
+        <link rel="shortcut icon" href="img/fevicon.png">
+
+        <!--Bootstrap Css-->
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+
+        <link rel="stylesheet" href="css/magnific-popup.css">
+
+        <!-- Animate Css -->
+        <link rel="stylesheet" href="css/animate.min.css">
+
+        <!-- Materialdesign icons Css -->
+        <link href="css/materialdesignicons.min.css" rel="stylesheet">
+
+        <!-- pe-icon-7 css -->
+        <link href="css/pe-icon-7.css" rel="stylesheet">
+
+        <!-- Owl Slider -->
+        <link rel="stylesheet" href="css/owl.carousel.css" />
+        <link rel="stylesheet" href="css/owl.theme.css" />
+        <link rel="stylesheet" href="css/owl.transitions.css" />
+
+
+        <!-- Custom style Css -->
+        <link href="css/style.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+
+        <!--Navbar Start-->
+        <nav class="navbar navbar-expand-lg fixed-top custom-nav sticky">
+            <div class="container">
+                <!-- LOGO -->
+                <a class="logo navbar-brand" href="index-2.html">
+                    <img src="img/logo.png" alt="" class="img-fluid logo-light"> 
+                    <img src="img/logo-dark.png" alt="" class="img-fluid logo-dark">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="mdi mdi-menu"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ml-auto navbar-center" id="mySidenav">
+                        <li class="nav-item active">
+                            <a href="#home" class="nav-link">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#about" class="nav-link">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#services" class="nav-link">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#resume" class="nav-link">Resume</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#work" class="nav-link">Work</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#client" class="nav-link">Client</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#blog" class="nav-link">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#contact" class="nav-link">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Navbar End -->
